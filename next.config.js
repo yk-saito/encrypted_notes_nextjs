9a81a59ae3f704a030b4ceafb8324310146aae7b
Yuka Saito <yksaito02@gmail.com>
2022-12-19 20:38:47 +0900

    mod config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,27 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+const DFXWebPackConfig = require("./dfx.webpack.config")
+DFXWebPackConfig.initCanisterEnv()
+
+const webpack = require("webpack")
+
+// Make DFX_NETWORK available to Web Browser with default "local" if DFX_NETWORK is undefined
+// const EnvPlugin = new webpack.EnvironmentPlugin({
+//   DFX_NETWORK: "local"
+// })
+
+module.exports = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Plugin
+    // config.plugins.push(EnvPlugin)
+
+    // Important: return the modified config
+    return config
+  }
+}
